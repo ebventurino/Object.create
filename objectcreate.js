5078e920de010c9defb7073da49d0a03857a16f6
Emily Venturino <ebventurino@gmail.com>
2018-06-01 09:40:52 -0500

    first commit

---
+++ objectcreate.js
@@ -0,0 +1,17 @@
+const Firstlight = Object.create({}, {
+    business: {
+        value: "Firstlight Arts Academy"
+    },
+    employmentStart: {
+        value: "03-01-2015"
+    },
+    employmentEnd: {
+        value: "05-01-2018"
+    },
+    pros: {
+        value: "Working with art supplies"
+    },
+    cons: {
+        value: "working with kids"
+    }
+})
