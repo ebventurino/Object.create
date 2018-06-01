f318d31bb3d857bbd72262dda6132f6f09daaaf9
Emily Venturino <ebventurino@gmail.com>
2018-06-01 12:41:05 -0500

    started second challenge

--- objectcreate.js
+++ objectcreate.js
@@ -1,21 +1,84 @@
-const Firstlight = Object.create({}, {
+
+
+const firstlight = Object.create({}, {
     business: {
+        enumerable:true,
+        writable:true,
         value: "Firstlight Arts Academy"
     },
     employmentStart: {
+        enumerable: true,
+        writable: false,
         value: "03-01-2015"
     },
     employmentEnd: {
+        enumerable: true,
+        writable: false,
         value: "05-01-2018"
     },
     pros: {
+        enumerable: true,
+        writable: false,
         value: "Working with art supplies"
     },
     cons: {
+        enumerable: true,
+        writable: false,
         value: "working with kids"
     }
 })
+const ddavis = Object.create({}, {
+    business: {
+        enumerable:true,
+        writable:true,
+        value: "Dennis Davis"
+    },
+    employmentStart: {
+        enumerable: true,
+        writable: false,
+        value: "03-01-2015"
+    },
+    employmentEnd: {
+        enumerable: true,
+        writable: false,
+        value: "05-01-2018"
+    },
+    pros: {
+        enumerable: true,
+        writable: false,
+        value: "Working with websites"
+    },
+    cons: {
+        enumerable: true,
+        writable: false,
+        value: "working with parents"
+    }
+})
 
+const market = Object.create({}, {
+    employmentStart: {
+        enumerable: true,
+        writable: false,
+        value: "03-01-2013"
+    },
+    employmentEnd: {
+        enumerable: true,
+        writable: false,
+        value: "05-01-2014"
+    },
+    pros: {
+        enumerable: true,
+        writable: false,
+        value: "Working with product"
+    },
+    cons: {
+        enumerable: true,
+        writable: false,
+        value: "answering phones"
+  }
+}) 
+
+console.log(firstlight)
 // CHALLENGE
 
 
@@ -44,3 +107,15 @@ const JobMaker = (business,employmentStart,employmentEnd) => {
 }
 
 JobMaker("teacher","03-01-2015","05-01-2018")
+
+
+// ADVANCED CHALLENGE
+
+const jobs = [firstlight, ddavis, market]
+
+for(let i = 0; i < jobs.length; i++) {
+    let article = document.createElement("article")
+    article.id = firstlight.business.value
+    console.log(article)
+}
+
