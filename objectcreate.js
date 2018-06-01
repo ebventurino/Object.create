74f40abaf89601a142ad619d36b08bbffb871a39
Emily Venturino <ebventurino@gmail.com>
2018-06-01 10:48:02 -0500

    finished challenge

--- objectcreate.js
+++ objectcreate.js
@@ -15,3 +15,32 @@ const Firstlight = Object.create({}, {
         value: "working with kids"
     }
 })
+
+// CHALLENGE
+
+
+const JobMaker = (business,employmentStart,employmentEnd) => {
+    const theObjectYouBuilt = Object.create(null, {
+        job: {
+                enumerable: true,
+                writable: false,
+                value: business
+            },
+        start: {
+                enumerable: true,
+                writable: false,
+                value: employmentStart
+            },
+        end: {
+                enumerable: true,
+                writable: false,
+                value:employmentEnd
+            }
+
+    }) 
+    return (console.log(theObjectYouBuilt))
+    
+    
+}
+
+JobMaker("teacher","03-01-2015","05-01-2018")
